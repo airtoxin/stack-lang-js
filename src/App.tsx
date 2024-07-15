64cb6830e2b1630fd9937e46669416d455c7632f
airtoxin <airtoxin@icloud.com>
2024-07-15 12:01:52 +0900

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,137 @@
-import { useCallback, useState } from "react";
-import { StackLang } from "./StackLang.ts";
+import {
+  FunctionComponent,
+  PropsWithChildren,
+  useCallback,
+  useState,
+} from "react";
+import { StackLang, Value, VmState } from "./StackLang.ts";
+
+const initialInput = `\
+/x 10 def
+/y { 20 } def
+/xy { x y + } def
+
+xy x *
+puts
+`;
 
 function App() {
-  const [input, setInput] = useState<string>("");
-  const handleClickRun = useCallback(() => {
-    new StackLang(input).run();
+  const reset = useCallback(() => {
+    setOutputs([]);
+    setVmState({ stack: [], vars: [], blocks: [] });
+  }, []);
+  const [input, setInput] = useState<string>(initialInput);
+  const [outputs, setOutputs] = useState<string[]>([]);
+  const [vmState, setVmState] = useState<VmState>({
+    stack: [],
+    vars: [],
+    blocks: [],
+  });
+  const handleClickRun = useCallback(async () => {
+    for (const vmEvent of new StackLang(input).run()) {
+      if (vmEvent.kind === "state") {
+        setVmState({ ...vmEvent.state });
+      } else if (vmEvent.kind === "stackPop") {
+        setVmState(vmEvent.state);
+        setOutputs((os) =>
+          [`pop ${serializeValue(vmEvent.value)}`].concat(os).slice(0, 5),
+        );
+      } else if (vmEvent.kind === "puts") {
+        setOutputs((os) =>
+          [`puts ${serializeValue(vmEvent.value)}`].concat(os).slice(0, 5),
+        );
+      }
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
   }, [input]);
 
   return (
-    <div>
-      <textarea
-        name="program"
-        value={input}
-        rows={10}
-        onChange={(event) => setInput(event.target.value)}
-      ></textarea>
-      <button onClick={handleClickRun}>Run</button>
-    </div>
+    <main className="h-dvh flex gap-2">
+      <div className="w-1/2 flex flex-col-reverse">
+        <textarea
+          className="h-full border rounded font-mono p-1 resize-none"
+          name="program"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button className="border rounded hover:bg-amber-50" onClick={reset}>
+          Reset
+        </button>
+        <button
+          className="border rounded hover:bg-amber-50"
+          onClick={handleClickRun}
+        >
+          Run
+        </button>
+      </div>
+      <div className="w-1/2 flex flex-col gap-2 [&>*]:m-1 [&>*]:h-full [&>*]:overflow-scroll">
+        <VmStateSection label="output">
+          {outputs.map((output) => (
+            <p>{output}</p>
+          ))}
+        </VmStateSection>
+        <VmStateSection label="stack">
+          {vmState.stack.toReversed().map((v) => (
+            <div>{serializeValue(v)}</div>
+          ))}
+        </VmStateSection>
+        <VmStateSection label="vars">
+          <ol>
+            {vmState.vars.toReversed().map((variableScope) => (
+              <li className="border p-2">
+                <ul className="list-inside list-disc">
+                  {Object.entries(variableScope).map(([k, v]) => (
+                    <li key={k}>
+                      {k} = {serializeValue(v)}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ol>
+        </VmStateSection>
+        <VmStateSection label="blocks">
+          <ol>
+            {vmState.blocks.toReversed().map((blockCode) => (
+              <li className="border p-2">
+                <ul className="list-inside list-disc">
+                  {blockCode.toReversed().map((v) => (
+                    <li>{serializeValue(v)}</li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ol>
+        </VmStateSection>
+      </div>
+    </main>
   );
 }
+
+const serializeValue = (value: Value): string => {
+  switch (value.kind) {
+    case "Num":
+      return "" + value.num;
+    case "Native":
+      return `<Native>`;
+    case "Symbol":
+      return value.sym;
+    case "Operator":
+      return value.op;
+    case "Block":
+      return `{ ${value.values.map((v) => serializeValue(v)).join(" ")} }`;
+  }
+};
+
+const VmStateSection: FunctionComponent<
+  PropsWithChildren<{ label: string }>
+> = ({ label, children }) => (
+  <section className="p-2 bg-blue-950 text-amber-50">
+    <label className="inline-block px-1 rounded-br font-bold mb-2 bg-white text-blue-950 -translate-x-2 -translate-y-2">
+      {label}
+    </label>
+    {children}
+  </section>
+);
 
 export default App;
--- src/StackLang.ts
+++ src/StackLang.ts
@@ -2,110 +2,115 @@ export class StackLang {
   constructor(private readonly program: string) {}
   private readonly vm = new Vm();
 
-  run(): void {
+  *run(): Generator<VmEvent> {
+    yield this.vm.stateEvent();
     for (const line of this.program.split("\n")) {
       for (const word of line.split(" ")) {
-        this.vm.parse(word);
+        yield* this.vm.parse(word);
       }
     }
   }
 }
 
+export type VmEvent =
+  | { kind: "state"; state: VmState }
+  | { kind: "puts"; value: Value }
+  | { kind: "stackPop"; state: VmState; value: Value };
+
+export type VmState = {
+  stack: Array<Value>;
+  vars: Array<Record<string, Value>>;
+  blocks: Array<Array<Value>>;
+};
+
 class Vm {
-  private stack: Array<Value> = [];
-  private vars: Array<Map<string, Value>> = [
-    new Map([
-      [
-        "+",
-        {
+  private state: VmState = {
+    stack: [],
+    vars: [
+      {
+        "+": {
           kind: "Native",
           fn: this.add.bind(this),
         },
-      ],
-      [
-        "-",
-        {
+        "-": {
           kind: "Native",
           fn: this.sub.bind(this),
         },
-      ],
-      [
-        "*",
-        {
+        "*": {
           kind: "Native",
           fn: this.mul.bind(this),
         },
-      ],
-      [
-        "/",
-        {
+        "/": {
           kind: "Native",
           fn: this.div.bind(this),
         },
-      ],
-      [
-        "<",
-        {
+        "<": {
           kind: "Native",
           fn: this.lt.bind(this),
         },
-      ],
-      [
-        "if",
-        {
+        if: {
           kind: "Native",
           fn: this.opIf.bind(this),
         },
-      ],
-      [
-        "def",
-        {
+        def: {
           kind: "Native",
           fn: this.opDef.bind(this),
         },
-      ],
-      [
-        "puts",
-        {
+        exch: {
+          kind: "Native",
+          fn: this.opExch.bind(this),
+        },
+        puts: {
           kind: "Native",
           fn: this.puts.bind(this),
         },
-      ],
-    ]),
-  ];
-  private blocks: Array<Array<Value>> = [];
+      },
+    ],
+    blocks: [],
+  };
 
-  public parse(word: string): void {
-    if (word === "") return;
+  public stateEvent(): VmEvent {
+    return { kind: "state", state: this.state };
+  }
+
+  private stackPopEvent(value: Value | undefined): VmEvent {
+    if (value == null) throw new Error(`Stack pop error`);
+    return { kind: "stackPop", state: this.state, value };
+  }
+
+  public *parse(word: string): Generator<VmEvent> {
+    if (word === "") return yield this.stateEvent();
 
     if (word === "{") {
-      this.blocks.push([]);
+      this.state.blocks.push([]);
+      yield this.stateEvent();
     } else if (word === "}") {
-      const values = this.blocks.pop();
+      const values = this.state.blocks.pop();
+      yield this.stateEvent();
       if (values == null) throw new Error(`Block stack underflow`);
-      this.eval({ kind: "Block", values });
+      yield* this.eval({ kind: "Block", values });
     } else {
       const num = Number.parseFloat(word);
       if (!Number.isNaN(num)) {
-        this.eval({ kind: "Num", num });
+        yield* this.eval({ kind: "Num", num });
       } else if (word.startsWith("/")) {
-        this.eval({ kind: "Symbol", sym: word.slice(1) });
+        yield* this.eval({ kind: "Symbol", sym: word.slice(1) });
       } else {
-        this.eval({ kind: "Operator", op: word });
+        yield* this.eval({ kind: "Operator", op: word });
       }
     }
   }
 
-  private eval(code: Value): void {
+  private *eval(code: Value): Generator<VmEvent> {
     // ブロック中のコードはBlockの内容を後でまとめてevalするためにblocksに積んでおく
-    if (this.blocks.length !== 0) {
-      this.blocks.at(-1)!.push(code);
-      return;
+    if (this.state.blocks.length !== 0) {
+      this.state.blocks.at(-1)!.push(code);
+      return yield this.stateEvent();
     }
     // stackを操作するのはOperatorのみなので、それ以外はstackに積むだけ
     if (code.kind !== "Operator") {
-      this.stack.push(code);
-      return;
+      this.state.stack.push(code);
+      return yield this.stateEvent();
     }
 
     const variableValue = this.findVar(code.op);
@@ -113,30 +118,35 @@ class Vm {
       throw new Error(`${code.op} is not a defined operation`);
 
     if (variableValue.kind === "Block") {
-      this.vars.push(new Map());
+      this.state.vars.push({});
+      yield this.stateEvent();
       for (const v of variableValue.values) {
-        this.eval(v);
+        yield* this.eval(v);
       }
-      this.vars.pop();
+      this.state.vars.pop();
+      yield this.stateEvent();
     } else if (variableValue.kind === "Native") {
-      variableValue.fn();
+      yield* variableValue.fn();
     } else {
-      this.stack.push(variableValue);
+      this.state.stack.push(variableValue);
+      yield this.stateEvent();
     }
   }
 
   private findVar(word: string): Value | null {
     return (
-      this.vars.toReversed().flatMap((varMap) => {
-        const v = varMap.get(word);
+      this.state.vars.toReversed().flatMap((varMap) => {
+        const v = varMap[word];
         return v ?? [];
       })[0] ?? null
     );
   }
 
-  private add() {
-    const right = this.stack.pop();
-    const left = this.stack.pop();
+  private *add(): Generator<VmEvent> {
+    const right = this.state.stack.pop();
+    yield this.stackPopEvent(right);
+    const left = this.state.stack.pop();
+    yield this.stackPopEvent(left);
     if (
       right == null ||
       left == null ||
@@ -144,12 +154,15 @@ class Vm {
       left.kind !== "Num"
     )
       throw new Error(`Expect Num Num +, (${left} ${right} +)`);
-    this.stack.push({ kind: "Num", num: left.num + right.num });
+    this.state.stack.push({ kind: "Num", num: left.num + right.num });
+    yield this.stateEvent();
   }
 
-  private sub() {
-    const right = this.stack.pop();
-    const left = this.stack.pop();
+  private *sub(): Generator<VmEvent> {
+    const right = this.state.stack.pop();
+    yield this.stackPopEvent(right);
+    const left = this.state.stack.pop();
+    yield this.stackPopEvent(left);
     if (
       right == null ||
       left == null ||
@@ -157,12 +170,15 @@ class Vm {
       left.kind !== "Num"
     )
       throw new Error(`Expect Num Num -, (${left} ${right} -)`);
-    this.stack.push({ kind: "Num", num: left.num - right.num });
+    this.state.stack.push({ kind: "Num", num: left.num - right.num });
+    yield this.stateEvent();
   }
 
-  private mul() {
-    const right = this.stack.pop();
-    const left = this.stack.pop();
+  private *mul(): Generator<VmEvent> {
+    const right = this.state.stack.pop();
+    yield this.stackPopEvent(right);
+    const left = this.state.stack.pop();
+    yield this.stackPopEvent(left);
     if (
       right == null ||
       left == null ||
@@ -170,12 +186,15 @@ class Vm {
       left.kind !== "Num"
     )
       throw new Error(`Expect Num Num *, (${left} ${right} *)`);
-    this.stack.push({ kind: "Num", num: left.num * right.num });
+    this.state.stack.push({ kind: "Num", num: left.num * right.num });
+    yield this.stateEvent();
   }
 
-  private div() {
-    const right = this.stack.pop();
-    const left = this.stack.pop();
+  private *div(): Generator<VmEvent> {
+    const right = this.state.stack.pop();
+    yield this.stackPopEvent(right);
+    const left = this.state.stack.pop();
+    yield this.stackPopEvent(left);
     if (
       right == null ||
       left == null ||
@@ -183,12 +202,15 @@ class Vm {
       left.kind !== "Num"
     )
       throw new Error(`Expect Num Num /, (${left} ${right} /)`);
-    this.stack.push({ kind: "Num", num: left.num / right.num });
+    this.state.stack.push({ kind: "Num", num: left.num / right.num });
+    yield this.stateEvent();
   }
 
-  private lt() {
-    const right = this.stack.pop();
-    const left = this.stack.pop();
+  private *lt(): Generator<VmEvent> {
+    const right = this.state.stack.pop();
+    yield this.stackPopEvent(right);
+    const left = this.state.stack.pop();
+    yield this.stackPopEvent(left);
     if (
       right == null ||
       left == null ||
@@ -196,62 +218,88 @@ class Vm {
       left.kind !== "Num"
     )
       throw new Error(`Expect Num Num /, (${left} ${right} /)`);
-    this.stack.push({ kind: "Num", num: left.num < right.num ? 1 : 0 });
+    this.state.stack.push({ kind: "Num", num: left.num < right.num ? 1 : 0 });
+    yield this.stateEvent();
   }
 
-  private opIf() {
-    const falseBranch = this.stack.pop();
+  private *opIf(): Generator<VmEvent> {
+    const falseBranch = this.state.stack.pop();
+    yield this.stackPopEvent(falseBranch);
     if (falseBranch == null || falseBranch.kind !== "Block")
       throw new Error(`Expect block (${falseBranch})`);
-    const trueBranch = this.stack.pop();
+    const trueBranch = this.state.stack.pop();
+    yield this.stackPopEvent(trueBranch);
     if (trueBranch == null || trueBranch.kind !== "Block")
       throw new Error(`Expect block (${trueBranch})`);
-    const cond = this.stack.pop();
+    const cond = this.state.stack.pop();
+    yield this.stackPopEvent(cond);
     if (cond == null || cond.kind !== "Block")
       throw new Error(`Expect block (${cond})`);
 
     for (const v of cond.values) {
-      this.eval(v);
+      yield* this.eval(v);
     }
-    const condResult = this.stack.pop();
+    const condResult = this.state.stack.pop();
+    yield this.stackPopEvent(condResult);
     if (condResult == null || condResult.kind !== "Num")
       throw new Error(`Expect num (${condResult})`);
 
     if (condResult.num !== 0) {
       for (const v of trueBranch.values) {
-        this.eval(v);
+        yield* this.eval(v);
       }
     } else {
       for (const v of falseBranch.values) {
-        this.eval(v);
+        yield* this.eval(v);
       }
     }
   }
 
-  private opDef() {
-    const value = this.stack.pop();
+  private *opDef(): Generator<VmEvent> {
+    const value = this.state.stack.pop();
+    yield this.stackPopEvent(value);
     if (value == null) throw new Error(`Expect value but got undefined`);
-    this.eval(value);
-    const evalResult = this.stack.pop();
+    yield* this.eval(value);
+    const evalResult = this.state.stack.pop();
+    yield this.stackPopEvent(evalResult);
     if (evalResult == null) throw new Error(`Expect value but got undefined`);
 
-    const symbol = this.stack.pop();
+    const symbol = this.state.stack.pop();
+    yield this.stackPopEvent(symbol);
     if (symbol == null || symbol.kind !== "Symbol")
       throw new Error(`Expect symbol (${symbol})`);
 
-    this.vars.at(-1)?.set(symbol.sym, evalResult);
+    this.state.vars.at(-1)![symbol.sym] ??= evalResult;
+    yield this.stateEvent();
   }
 
-  private puts() {
-    const value = this.stack.pop();
+  private *opExch(): Generator<VmEvent> {
+    const last = this.state.stack.pop();
+    yield this.stackPopEvent(last);
+    if (last == null) throw new Error(`Expect value but got undefined`);
+
+    const second = this.state.stack.pop();
+    yield this.stackPopEvent(second);
+    if (second == null) throw new Error(`Expect value but got undefined`);
+    yield* this.eval(second);
+
+    this.state.stack.push(last);
+    yield this.stateEvent();
+    this.state.stack.push(second);
+    yield this.stateEvent();
+  }
+
+  private *puts(): Generator<VmEvent> {
+    const value = this.state.stack.pop();
+    yield this.stackPopEvent(value);
     if (value == null) throw new Error(`stack under-run`);
-    console.log(value);
+    yield { kind: "puts", value };
   }
 }
 
-type Value =
+export type Value =
   | { kind: "Num"; num: number }
   | { kind: "Operator"; op: string }
   | { kind: "Symbol"; sym: string }
   | { kind: "Block"; values: readonly Value[] }
-  | { kind: "Native"; fn: () => void };
+  | { kind: "Native"; fn: () => Generator<VmEvent> };
